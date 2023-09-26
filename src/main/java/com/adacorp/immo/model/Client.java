7e17458c519423b2ceec7c13580094b6c7d49369
Stephane YOT <merlinstephane9@gmail.com>
2023-09-26 06:14:18 +0200

    commit avant de recuperer andreas

--- src/main/java/com/adacorp/immo/model/Client.java
+++ src/main/java/com/adacorp/immo/model/Client.java
@@ -2,7 +2,6 @@ package com.adacorp.immo.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-<<<<<<< HEAD
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
@@ -18,30 +17,13 @@ import java.util.UUID;
 @NoArgsConstructor
 @ToString
 @EqualsAndHashCode
-=======
-import jakarta.persistence.Id;
-import lombok.*;
->>>>>>> a109e7cbc3f393e768c6178ef3faa850c42fe293
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-@EqualsAndHashCode
-@Entity
 public class Client {
     @Id
     @GeneratedValue
-<<<<<<< HEAD
     private UUID clientID;
     private String nomComplet;
     private String email;
 
 
-=======
-    private Integer clientID;
-    private String nomComplet;
-    private String email;
->>>>>>> a109e7cbc3f393e768c6178ef3faa850c42fe293
 }
